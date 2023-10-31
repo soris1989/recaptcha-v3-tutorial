0c5ea828022312949498157e9a4e0d4f1c605f19
DESKTOP-SK1K6UJ\ori <ori.sarah.sa@gmail.com>
2023-10-31 23:47:11 +0200

    safe commit

--- constant.php
+++ constant.php
@@ -1,5 +1,5 @@
 <?php
-//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
+// dummy reCAPTCHA Configuration for test - go to Google and get the below keys http://www.google.com/recaptcha/admin
 define('SITE_KEY','6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'); 
 define('SECRET_KEY','6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe');
 ?>
